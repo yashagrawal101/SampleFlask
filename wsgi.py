52eb4adb69de49241b7a15b28a4dcd4c059f5ec6
yashagrawal101 <58508158+yashagrawal101@users.noreply.github.com>
2021-09-03 09:40:51 +1000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! Webhook testing"
+    return "Hello World! Webhook testing! Github Edit"
 
 if __name__ == "__main__":
     application.run()
