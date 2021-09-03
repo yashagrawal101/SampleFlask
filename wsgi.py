2d9a8e9c309cbfb579ea44fface34d52c5ea6e1b
yash13 <yashbusiness101@gmail.com>
2021-09-03 00:13:17 +0000

    First html file

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
+# @application.route("/")
+# def hello():
+#     return "Hello World! Webhook testing! Github Edit"
+
 @application.route("/")
-def hello():
-    return "Hello World! Webhook testing! Github Edit"
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     application.run()
