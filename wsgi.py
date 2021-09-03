3f9b01a895410d6c89a1f33ad8e06350d309f5d5
yash13 <yashbusiness101@gmail.com>
2021-09-03 00:56:09 +0000

    Addition of multiple html files

--- wsgi.py
+++ wsgi.py
@@ -5,9 +5,21 @@ application = Flask(__name__)
 # def hello():
 #     return "Hello World! Webhook testing! Github Edit"
 
+
 @application.route("/")
+def template():
+    return render_template("template.html")
+
+
+@application.route("/home")
 def home():
     return render_template("home.html")
+
+
+@application.route("/about")
+def about():
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
     application.run()
